d15f3050ea1cead688e4490fea6d4bbe04d8adb1
Ajayb991 <132124479+Ajayb991@users.noreply.github.com>
2023-06-23 16:10:40 +0530

    Update server.js
    new server.js file

--- server/server.js
+++ server/server.js
@@ -1,17 +1,21 @@
-import config from './../config/config'
-import app from './express'
-import mongoose from 'mongoose'
+import config from './../config/config';
+import app from './express';
+import mongoose from 'mongoose';
 
 // Connection URL
-mongoose.Promise = global.Promise
-mongoose.connect(config.mongoUri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true, useFindAndModify: false })
-mongoose.connection.on('error', () => {
-  throw new Error(`unable to connect to database: ${config.mongoUri}`)
+mongoose.connect(config.mongoUri, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false
 })
-
-app.listen(config.port, (err) => {
-  if (err) {
-    console.log(err)
-  }
-  console.info('Server started on port %s.', config.port)
-})
+  .then(() => {
+    console.log('Connected to MongoDB');
+    app.listen(config.port, () => {
+      console.info('Server started on port', config.port);
+    });
+  })
+  .catch((err) => {
+    console.error('Failed to connect to MongoDB:', err);
+    process.exit(1); // Exit the process with a failure code
+  });
